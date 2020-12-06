7c789ad22237d5e39e2ed8ffd703efafc9a6e5aa
Qiaoyw <63182186+Qiaoyw@users.noreply.github.com>
2020-12-06 20:10:31 +0800

    Add files via upload

---
+++ BigC0/src/main/java/BigC0/App.java
@@ -0,0 +1,20 @@
+package BigC0;
+
+import BigC0.analyser.Analyser;
+import BigC0.error.CompileError;
+import BigC0.tokenizer.StringIter;
+import BigC0.tokenizer.Tokenizer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws FileNotFoundException, CompileError {
+        Scanner sc = new Scanner(new File(args[0]));
+        StringIter it = new StringIter(sc);
+        Tokenizer tn = new Tokenizer(it);
+        Analyser analyser = new Analyser(tn);
+        analyser.Analyse();
+    }
+}
